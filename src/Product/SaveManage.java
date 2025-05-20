551407ccc431ea7ac6b50d9a8d0d967cf4ff6ad5
papuc <papucapatrik@gmail.com>
2025-05-20 17:44:05 +0200

    pridani save/load

---
+++ src/Product/SaveManage.java
@@ -0,0 +1,65 @@
+package Product;
+
+import Product.Coin;
+import Product.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class SaveManage {
+
+    public static void saveGame(Coin coin, Product[] products, ArrayList<Product> storageList) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("save.txt"))) {
+            writer.write(String.valueOf(coin.getCoins()));
+            writer.newLine();
+
+            for (int i = 0; i < products.length; i++) {
+                if (products[i] != null) {
+                    writer.write(String.valueOf(products[i].getLevel()));
+                } else {
+                    writer.write("0");
+                }
+                if (i < products.length - 1) {
+                    writer.write(",");
+                }
+            }
+            writer.newLine();
+            for (int i = 0; i < storageList.size(); i++) {
+                writer.write(String.valueOf(storageList.get(i).getLevel()));
+                if (i < storageList.size() - 1) {
+                    writer.write(",");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadGame(Coin coin, Product[] product, ArrayList<Product> storageList) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("save.txt"))) {
+
+            int coins = Integer.parseInt(reader.readLine());
+            coin.setCoins(coins);
+
+
+            String[] productLevel = reader.readLine().split(",");
+            for (int i = 0; i < productLevel.length && i < productLevel.length; i++) {
+                int lvl = Integer.parseInt(productLevel[i]);
+                product[i] = (lvl > 0) ? new Product(lvl) : null;
+            }
+
+            storageList.clear();
+            String storageLine = reader.readLine();
+            if (storageLine != null && !storageLine.isEmpty()) {
+                String[] storageLevels = storageLine.split(",");
+                for (String s : storageLevels) {
+                    int lvl = Integer.parseInt(s);
+                    storageList.add(new Product(lvl));
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("No save file found.");
+        }
+    }
+}
