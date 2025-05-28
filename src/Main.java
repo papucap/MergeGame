96eba36096245eebd035fb0b310a81147969c26e
papuc <papucapatrik@gmail.com>
2025-05-28 23:03:22 +0200

    komentare Main

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ import Product.Coin;
 import Product.Statistics;
 import Product.Storage;
 
+// Main class to start the application
 public class Main {
     public static void main(String[] args) {
 
         Coin coin = new Coin(20000);
 
-        MainMenu mainMenu = new MainMenu(coin);
+        MainMenu mainMenu = new MainMenu(coin);  // Initialize the main menu
     }
 }
