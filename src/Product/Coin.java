6d9ef3224a90ebee1778535d6e3deca893beca2e
papuc <papucapatrik@gmail.com>
2025-05-28 22:54:10 +0200

    komentare v Coin

--- src/Product/Coin.java
+++ src/Product/Coin.java
@@ -1,5 +1,6 @@
 package Product;
 
+// Coin class represents the currency in the game
 public class Coin {
     private int coins;
 
@@ -7,21 +8,24 @@ public class Coin {
         this.coins = coins;
     }
 
-
+    // Method to buy an item, deducting the specified amount from coins
     public void buy (int amount){
         if (coins >= amount){
             setCoins(coins - amount);
         }
     }
 
+    // Method to sell an item, adding the specified amount to coins
     public void sell (int amount){
         setCoins(coins+amount);
     }
 
+    // Getter method to retrieve the current amount of coins
     public int getCoins() {
         return coins;
     }
 
+    // Setter method to update the amount of coins
     public void setCoins(int coins) {
         this.coins = coins;
     }
